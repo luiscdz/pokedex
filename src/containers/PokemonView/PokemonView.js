8f589130c0f3193f64b4e779eda08a37c0dc85d1
Luis Cardozo <luisedu.cdz@gmail.com>
2019-10-27 16:40:54 -0500

    feat(PokemonView): added connection with the service api

--- src/containers/PokemonView/PokemonView.js
+++ src/containers/PokemonView/PokemonView.js
@@ -1,17 +1,50 @@
 
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Layout from '../../components/Layout/Layout'
 import PokemonList from '../../components/PokemonList/PokemonList'
 import PokemonDetail from '../../components/PokemonDetail/PokemonDetail'
+import Header from '../../components/Header/Header';
+import { getPokemon } from '../../services/pokemonApi';
 import './PokemonView.css';
 
 class PokemonView extends Component {
+  state = {
+    pokemonlist: [],
+    pokemonSelected: {}
+  }
+
+  componentDidMount() {
+    this.getAllPokemon();
+  }
+
+  getAllPokemon() {
+    const that = this;
+    getPokemon()
+    .then(list => {
+      console.log(list);
+      that.setState({
+        pokemonlist: list
+      });
+    }); 
+  }
+
+  openPokemonDetail = (pokemon) => {
+    this.setState({
+      pokemonSelected: pokemon
+    });
+  }
+
   render() {
+    const { pokemonlist, pokemonSelected } = this.state;
+    
     return (
-      <Layout className="PokemonView">
-        <PokemonList />
-        <PokemonDetail />
-      </Layout>
+      <Fragment>
+        <Header />
+        <Layout className="PokemonView">
+          <PokemonList list={pokemonlist} openPokemonDetail={this.openPokemonDetail} pokemonSelected={pokemonSelected} />
+          <PokemonDetail />
+        </Layout>
+      </Fragment>
     )
   }
 }
