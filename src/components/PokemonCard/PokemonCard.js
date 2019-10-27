fd8a8a28f646f68a6bb875f54d18d64cc1fa02f3
Luis Cardozo <luisedu.cdz@gmail.com>
2019-10-27 16:32:29 -0500

    enhancement(PokemonCard): added new styles and more pokemon detail

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import PokemonThumbnail from '../PokemonThumbnail/PokemonThumbnail';
 import './PokemonCard.css';
+import PokemonType from '../PokemonType/PokemonType';
 
-const PokemonCard = ({ image, name }) => {
+const PokemonCard = ({ 
+  pathImage,
+  name,
+  isSelected,
+  handleClick,
+  number,
+  types
+}) => {
   return (
-    <div className="PokemonCard">
-      <img alt="Pokémon" src={image} className="PokemonCard__img" />
-      <div class="PokemonCard__contentLabel">
+    <div
+      className={`PokemonCard ${isSelected ? 'PokemonCard--selected' : '' }`}
+      onClick={handleClick}
+    >
+      <PokemonThumbnail alt="Pokémon" pathImage={pathImage} className="PokemonCard__img" />
+      <div className="PokemonCard__contentTypes">
+        {
+          types && types.map(type => (<PokemonType type={type.type.name} />))
+        }
+      </div>
+      <div className="PokemonCard__contentLabel">
         <label className="PokemonCard__label">{name}</label>
-      </div>      
+      </div>
+      <span className="PokemonCard__number">#{number}</span>
     </div>
   )
 }
